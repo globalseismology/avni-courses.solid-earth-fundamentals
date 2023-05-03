c8150b2e7072a1551d977d40ef3b0ab865af2327
Pritwiraj Moulik <moulik@caa.columbia.edu>
2023-05-02 22:21:32 -0400

    modified

---
+++ middleware/setupNotebook.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+toolName = 'solidearth'
+notebookName = 'welcome.ipynb'
+
+try:
+   toolParametersPath = os.environ['TOOL_PARAMETERS']
+except:
+   pass
+else:
+   if os.path.exists(toolParametersPath):
+      try:
+         with open(toolParametersPath, 'r') as fp:
+            toolParameters = fp.readlines()
+      except IOError as e:
+         sys.stderr.write("%s: %s\n" % (e.strerror,e.filename))
+      else:
+         for record in toolParameters:
+            label,notebookPath = record.split(':')
+            if label == 'file(notebook)':
+               if notebookPath.startswith(os.path.join(os.sep,'apps',toolName)):
+                  notebookPath = notebookPath.strip()
+                  if notebookPath.endswith('.ipynb'):
+                     notebookName = os.path.basename(notebookPath)
+                     break
+
+if notebookName:
+   print(notebookName)
